97442a76673b034d8824ce02dc3f57f9387ef6ad
Divya <riktam@Riktams-MacBook-Air.local>
2026-08-10 15:46:51 +0530

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,38 @@
+const testRoutes = require("./routes/testRoutes");
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+
+
+// Connect MongoDB
+connectDB();
+
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("API Running");
+});
+
+// Authentication routes
+app.use("/api/auth", authRoutes);
+
+app.use("/api/test", testRoutes);
+
+
+// Start server
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
